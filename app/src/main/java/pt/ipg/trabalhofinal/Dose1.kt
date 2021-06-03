1452716f4d807bc85284dc1e0fb23c17d1f0c883
Daneil Carmona <carmona240101@hotmail.com>
2021-06-03 15:44:14 +0100

    criação da classe dose1

--- app/src/main/java/pt/ipg/trabalhofinal/Dose1.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/Dose1.kt
@@ -1,4 +1,33 @@
 package pt.ipg.trabalhofinal
 
-class Dose1 {
+import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+
+data class Dose1 (var id: Long = -1, var data: Int, var idUtente: Long, var idVacina: Long) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues().apply {
+            put(TabelaDose1.CAMPO_DATA_ADMINISTRACAO, data)
+            put(TabelaDose1.CAMPO_ID_UTENTE, idUtente)
+            put(TabelaDose1.CAMPO_ID_VACINA, idVacina)
+        }
+
+        return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Dose2 {
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colData = cursor.getColumnIndex(TabelaDose1.CAMPO_DATA_ADMINISTRACAO)
+            val colIdUtente = cursor.getColumnIndex(TabelaDose1.CAMPO_ID_UTENTE)
+            val colIdVacina = cursor.getColumnIndex(TabelaDose1.CAMPO_ID_VACINA)
+
+            val id = cursor.getLong(colId)
+            val data = cursor.getInt(colData)
+            val idUtente = cursor.getLong(colIdUtente)
+            val idVacina = cursor.getLong(colIdUtente)
+
+            return Dose2(id, data, idUtente, idVacina)
+        }
+    }
 }
