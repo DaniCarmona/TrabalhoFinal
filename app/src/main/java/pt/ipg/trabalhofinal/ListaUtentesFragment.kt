46a1a13d528cc20ab02c0ff74f9e8f20ca0628a5
Daneil Carmona <carmona240101@hotmail.com>
2021-06-18 00:28:41 +0100

    implementado ListaUtentesFragment para ver a lista de utentes

--- app/src/main/java/pt/ipg/trabalhofinal/ListaUtentesFragment.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/ListaUtentesFragment.kt
@@ -1,20 +1,27 @@
 package pt.ipg.trabalhofinal
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import pt.ipg.trabalhofinal.databinding.FragmentListaUtentesBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class ListaUtentesFragment : Fragment() {
+class ListaUtentesFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     private var _binding: FragmentListaUtentesBinding? = null
+    private var adapterUtentes : AdapterUtentes? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,44 +31,150 @@ class ListaUtentesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        DadosApp.listaUtentesFragment = this
 
         _binding = FragmentListaUtentesBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val recyclerViewLivros = view.findViewById<RecyclerView>(R.id.recyclerViewUtentes)
+        adapterUtentes = AdapterUtentes(this)
+        recyclerViewLivros.adapter = adapterUtentes
+        recyclerViewLivros.layoutManager = LinearLayoutManager(requireContext())
 
-
-
+        LoaderManager.getInstance(this)
+            .initLoader(ID_LOADER_MANAGER_UTENTES, null, this)
     }
 
-    fun navegaNovoLivro() {
+    fun navegaNovoUtente() {
         //findNavController().navigate(R.id.action_ListaLivrosFragment_to_NovoLivroFragment)
+        //todo: navegar para o fragmento de novo utente
     }
 
-    fun navegaAlterarLivro() {
-        //todo: navegar para o fragmento da edição de um livro
+    fun navegaEditarUtente() {
+        //todo: navegar para o fragmento da edição de um utente
     }
 
-    fun navegaEliminarLivro() {
-        //todo: navegar para o fragmento para confirmar eliminação de um livro
+    fun navegaNovaDose() {
+        //todo: navegar para o fragmento para nova dose
+    }
+
+    fun navegaVerDosesUtente() {
+        //todo: navegar para o fragmento para ver as doses do utente
+    }
+
+    fun navegaVerTodasDoses() {
+        //todo: navegar para o fragmento para ver todas as doses
+    }
+
+    fun navegaVerStockVacinas() {
+        //todo: navegar para o fragmento para ver o sotck de vacinas
     }
 
     fun processaOpcaoMenu(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.action_novo_utente -> navegaNovoLivro()
-            R.id.action_editar_utente -> navegaAlterarLivro()
-            R.id.action_nova_dose -> navegaEliminarLivro()
+            R.id.action_novo_utente -> navegaNovoUtente()
+            R.id.action_editar_utente -> navegaEditarUtente()
+            R.id.action_nova_dose -> navegaNovaDose()
+            R.id.action_ver_doses_utente -> navegaVerDosesUtente()
+            R.id.action_ver_todas_doses -> navegaVerTodasDoses()
+            R.id.action_ver_stock_vacinas -> navegaVerStockVacinas()
             else -> return false
         }
 
         return true
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param id The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            ContentProviderArmazemVacinas.ENDERECO_UTENTES,
+            TabelaUtentes.TODOS_CAMPOS,
+            null, null,
+            TabelaUtentes.CAMPO_NOME
+        )
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is *not* allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
+     *
+     *
+     * This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     *
+     *
+     *  *
+     *
+     *The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a [android.database.Cursor]
+     * and you place it in a [android.widget.CursorAdapter], use
+     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
+     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
+     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     *  *  The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a [android.database.Cursor] from a [android.content.CursorLoader],
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * [android.widget.CursorAdapter], you should use the
+     * [android.widget.CursorAdapter.swapCursor]
+     * method so that the old Cursor is not closed.
+     *
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        adapterUtentes!!.cursor = data
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapterUtentes!!.cursor = null
+    }
+
+    companion object {
+        const val ID_LOADER_MANAGER_UTENTES = 0
     }
 }
