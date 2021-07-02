c41492820cfbf40a7ab2d3219f343466e7c47f57
Daniel Carmona <carmona240101@hotmail.com>
2021-07-02 12:16:02 +0100

    a data não fuinciona. tentar usar spinners

--- app/src/main/java/pt/ipg/trabalhofinal/NovoUtenteFragment.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/NovoUtenteFragment.kt
@@ -19,12 +19,15 @@ import java.util.*
 class NovoUtenteFragment : Fragment(){
 
     private var _binding: FragmentNovoUtenteBinding? = null
+    private var data: Date = Date(1900-1900, 0,0)
 
     private lateinit var editTextNome: EditText
     private lateinit var editTextTelefone: EditText
     private lateinit var editTextEmail: EditText
     private lateinit var editTextMorada: EditText
-    private lateinit var editTextDataNascimento: EditText
+    private lateinit var editTextDiaNascimento: EditText
+    private lateinit var editTextMesNascimento: EditText
+    private lateinit var editTextAnoNascimento: EditText
 
 
 
@@ -50,7 +53,9 @@ class NovoUtenteFragment : Fragment(){
         editTextTelefone = view.findViewById(R.id.editTextTelefone)
         editTextEmail = view.findViewById(R.id.editTextEmail)
         editTextMorada = view.findViewById(R.id.editTextMorada)
-       // editTextDataNascimento = view.findViewById(R.id.editTextDataNascimento)
+        editTextDiaNascimento = view.findViewById(R.id.editTextDiaNascimento)
+        editTextMesNascimento = view.findViewById(R.id.editTextMesNascimento)
+        editTextAnoNascimento = view.findViewById(R.id.editTextAnoNascimento)
 
     }
 
@@ -78,7 +83,7 @@ class NovoUtenteFragment : Fragment(){
             return
         }
 
-        val email = editTextNome.text.toString()
+        val email = editTextEmail.text.toString()
         if (email.isEmpty()) {
             editTextEmail.setError(getString(R.string.preencha_email))
             editTextEmail.requestFocus()
@@ -92,31 +97,47 @@ class NovoUtenteFragment : Fragment(){
             return
         }
 
-        val dataNascimento = editTextNome
-        /*if (dataNascimento.isEmpty()) {
-            editTextDataNascimento.setError(getString(R.string.preencha_data_nascimento))
-            editTextDataNascimento.requestFocus()
+        val diaNascimentoString = editTextDiaNascimento.toString()
+        val diaNascimento = diaNascimentoString.toInt()
+        if (diaNascimento == 0) {
+            editTextDiaNascimento.setError(getString(R.string.preencha_data_nascimento))
+            editTextDiaNascimento.requestFocus()
             return
-        }*/
+        }
+
+        val mesNascimentoString = editTextMesNascimento.toString()
+        val mesNascimento = mesNascimentoString.toInt()
+        if (mesNascimento == 0) {
+            editTextMesNascimento.setError(getString(R.string.preencha_data_nascimento))
+            editTextMesNascimento.requestFocus()
+            return
+        }
+        val anoNascimentoString = editTextAnoNascimento.toString()
+        val anoNascimento = anoNascimentoString.toInt()
+        if (anoNascimento == 0) {
+            editTextAnoNascimento.setError(getString(R.string.preencha_data_nascimento))
+            editTextAnoNascimento.requestFocus()
+            return
+        }
 
 
 
 
-      /*  val utente = Utente(nome = nome, telefone = telefone, email = email, morada = morada, dataNascimento = Date(dataNascimento), dose = 0)
+       val utente = Utente(nome = nome, telefone = telefone, email = email, morada = morada, dataNascimento = Date(anoNascimento,  mesNascimento-1, diaNascimento), dose = 0)
 
         val uri = activity?.contentResolver?.insert(
             ContentProviderArmazemVacinas.ENDERECO_UTENTES,
             utente.toContentValues()
-        )*/
+        )
 
-        /*if (uri == null) {
+        if (uri == null) {
             Snackbar.make(
                 editTextNome,
                 getString(R.string.erro_inserir_utente),
                 Snackbar.LENGTH_LONG
             ).show()
             return
-        }*/
+        }
 
         Toast.makeText(
             requireContext(),
