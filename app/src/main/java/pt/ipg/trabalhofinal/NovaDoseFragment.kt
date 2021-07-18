a9ca48acb88aca07886265de4afbaa31e3bbe0d6
Daneil Carmona <carmona240101@hotmail.com>
2021-07-18 23:18:50 +0100

    nova dose quase terminado

--- app/src/main/java/pt/ipg/trabalhofinal/NovaDoseFragment.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/NovaDoseFragment.kt
@@ -1,59 +1,211 @@
 package pt.ipg.trabalhofinal
 
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.widget.EditText
+import android.widget.SimpleCursorAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.navigation.fragment.findNavController
+import pt.ipg.trabalhofinal.databinding.FragmentNovaDoseBinding
+import java.util.*
 
 /**
- * A simple [Fragment] subclass.
- * Use the [NovaDoseFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class NovaDoseFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class NovaDoseFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentNovaDoseBinding? = null
+    private val data = Calendar.getInstance()
+
+    private lateinit var spinnerVacinas: Spinner
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nova_dose, container, false)
+        DadosApp.fragment = this
+        (activity as MainActivity).menuAtual = R.menu.menu_nova_dose
+
+        _binding = FragmentNovaDoseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        spinnerVacinas = view.findViewById(R.id.spinnerVacinas)
+
+        LoaderManager.getInstance(this)
+            .initLoader(ID_LOADER_MANAGER_VACINAS, null, this)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun navegaListaUtentes() {
+        findNavController().navigate(R.id.action_novaDoseFragment_to_ListaUtentesFragment)
+    }
+
+    fun guardar() {
+        val utente = DadosApp.utenteSelecionado!!
+        val idVacina = spinnerVacinas.selectedItemId
+        utente.dose = utente.dose+1
+
+        val dose = Dose(data = Date(data.get(Calendar.DATE),data.get(Calendar.MONTH),data.get(Calendar.YEAR)), dose = utente.dose, idUtente = DadosApp.utenteSelecionado!!.id, idVacina = idVacina)
+
+        val uri = activity?.contentResolver?.insert(
+            ContentProviderArmazemVacinas.ENDERECO_DOSES,
+            dose.toContentValues()
+        )
+
+        val uriUtente = Uri.withAppendedPath(
+            ContentProviderArmazemVacinas.ENDERECO_UTENTES,
+            utente.id.toString()
+        )
+
+        activity?.contentResolver?.update(
+            uriUtente,
+            utente.toContentValues(),
+            null,
+            null
+        )
+
+        if (uri == null) {
+            Toast.makeText(
+                requireContext(),
+                R.string.erro_inserir_dose,
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
+        Toast.makeText(
+            requireContext(),
+            R.string.dose_guardada_sucesso,
+            Toast.LENGTH_LONG
+        ).show()
+        navegaListaUtentes()
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_guardar_nova_dose -> guardar()
+            R.id.action_cancelar_nova_dose -> navegaListaUtentes()
+            else -> return false
+        }
+
+        return true
+    }
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param id The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            ContentProviderArmazemVacinas.ENDERECO_VACINAS,
+            TabelaVacinas.TODOS_CAMPOS,
+            null, null,
+            TabelaVacinas.CAMPO_NOME_VACINA
+        )
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is *not* allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See [ FragmentManager.openTransaction()][androidx.fragment.app.FragmentManager.beginTransaction] for further discussion on this.
+     *
+     *
+     * This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     *
+     *
+     *  *
+     *
+     *The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a [android.database.Cursor]
+     * and you place it in a [android.widget.CursorAdapter], use
+     * the [android.widget.CursorAdapter.CursorAdapter] constructor *without* passing
+     * in either [android.widget.CursorAdapter.FLAG_AUTO_REQUERY]
+     * or [android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER]
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     *  *  The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a [android.database.Cursor] from a [android.content.CursorLoader],
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * [android.widget.CursorAdapter], you should use the
+     * [android.widget.CursorAdapter.swapCursor]
+     * method so that the old Cursor is not closed.
+     *
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        atualizaSpinnerVacinas(data)
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        atualizaSpinnerVacinas(null)
+    }
+
+    private fun atualizaSpinnerVacinas(data: Cursor?) {
+        spinnerVacinas.adapter = SimpleCursorAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            data,
+            arrayOf(TabelaVacinas.CAMPO_NOME_VACINA),
+            intArrayOf(android.R.id.text1),
+            0
+        )
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NovaDoseFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NovaDoseFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val ID_LOADER_MANAGER_VACINAS = 0
     }
 }
